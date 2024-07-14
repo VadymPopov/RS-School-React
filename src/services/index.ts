5125985936cfbc2103dbd9194899bb03748e749b
VadymPopov <boycristal10@gmail.com>
2024-07-14 12:35:27 -0400

    feat: add pagination

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,7 +1,7 @@
 const baseUrl = 'https://swapi.dev/api/starships/';
 
-export async function getStarShips(query: string) {
-  const url = query ? baseUrl + `?search=${query}` : baseUrl;
+export async function getStarShips(query: string, page: string) {
+  const url = query ? baseUrl + `?search=${query}&page=${page}` : baseUrl;
   try {
     const response = await fetch(url);
     if (!response.ok) {
--- src/views/StarWars.tsx
+++ src/views/StarWars.tsx
@@ -4,17 +4,46 @@ import Button from '../components/Button';
 import InputField from '../components/InputField';
 import ShipsList from '../components/ShipsList';
 import Loader from '../components/Loader';
+import Pagination from '../components/Pagination';
 import useLocalStorage from '../hooks/useLocalStorage';
-import useFetchData from '../hooks/useFetchData';
 import { useNavigate } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { useSearchParams } from 'react-router-dom';
+import { getStarShips } from '../services';
+import { StarShip } from '../types';
 
 export default function StarWarsView({
   showSplitScreen,
 }: {
   showSplitScreen: boolean;
 }) {
+  const [searchParams, setSearchParams] = useSearchParams();
   const { searchQuery, setSearchQuery } = useLocalStorage();
-  const { starShips, error, loading, triggerError } = useFetchData(searchQuery);
+  const [error, setError] = useState<string>();
+  const [starShips, setStarShips] = useState<StarShip[]>([]);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [totalPages, setTotalPages] = useState<number>(0);
+  const page = searchParams.get('page') || '1';
+
+  useEffect(() => {
+    const fetchStarShips = async () => {
+      setLoading(true);
+      setError('');
+
+      try {
+        const data = await getStarShips(searchQuery, page);
+        setStarShips(data?.results);
+        setTotalPages(Math.round(Number(data?.count) / 10));
+      } catch (error: unknown) {
+        setError('Error fetching data');
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchStarShips();
+  }, [searchQuery, page]);
+
   const navigate = useNavigate();
   const handleLeftPaneClick = () => {
     navigate('/');
@@ -25,6 +54,14 @@ export default function StarWarsView({
     const form = e.target as HTMLFormElement;
     const searchQuery = form.query.value.trim();
     setSearchQuery(searchQuery);
+    setSearchParams({
+      search: searchQuery,
+      page: '1',
+    });
+  };
+
+  const triggerError = (): void => {
+    setError('This is a simulated error.');
   };
 
   if (error) {
@@ -50,6 +87,7 @@ export default function StarWarsView({
         ) : (
           <ShipsList ships={starShips} />
         )}
+        <Pagination totalPages={totalPages} />
       </section>
     </main>
   );
