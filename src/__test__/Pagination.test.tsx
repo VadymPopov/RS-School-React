a6e853e1016e8f4c034e0767b43b79c8d514cfab
VadymPopov <boycristal10@gmail.com>
2024-07-20 16:38:35 -0400

    feat: add test

--- src/__test__/Pagination.test.tsx
+++ src/__test__/Pagination.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen, fireEvent } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import Pagination from '../components/Pagination';
 import { MemoryRouter } from 'react-router-dom';
